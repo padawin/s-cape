b4cacd684dd15c66b6f46ee48d4c20fc1194660e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-25 22:16:39 +0100

    resources's hitboxes added

--- js/chase.js
+++ js/chase.js
@@ -14,9 +14,9 @@
 			// url, tile dimensions, top left position in grid's cell to be
 			// middle bottom aligned
 			'grass': {'url': 'resources/bg-grass.png', 'w': 40, 'h': 40},
-			'tree': {'url': 'resources/tree.png', 'w': 48, 'h': 48},
-			'player': {'url': 'resources/player.png', 'w': 32, 'h': 48},
-			'death': {'url': 'resources/death.png', 'w': 50, 'h': 48}
+			'tree': {'url': 'resources/tree.png', 'w': 48, 'h': 48, 'hitbox': [12, 24, 36, 48]},
+			'player': {'url': 'resources/player.png', 'w': 32, 'h': 48, 'hitbox': [12, 24, 36, 48]},
+			'death': {'url': 'resources/death.png', 'w': 50, 'h': 48, 'hitbox': [12, 24, 36, 48]}
 		},
 		_nbResources = 4,
 		_tileWidth = 48,
