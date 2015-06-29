c9b80b38b48768644558fa81b4f3348bc2861286
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-06-29 19:42:22 +0100

    the hitboxes are now x, y, width and height

--- js/chase.js
+++ js/chase.js
@@ -14,9 +14,9 @@
 			// url, tile dimensions, top left position in grid's cell to be
 			// middle bottom aligned
 			'grass': {'url': 'resources/bg-grass.png', 'w': 40, 'h': 40},
-			'tree': {'url': 'resources/tree.png', 'w': 48, 'h': 48, 'hitbox': [12, 24, 36, 48]},
-			'player': {'url': 'resources/player.png', 'w': 32, 'h': 48, 'cellChange': [16, 36], 'hitbox': [8, 24, 24, 48]},
-			'death': {'url': 'resources/death.png', 'w': 50, 'h': 48, 'cellChange': [25, 36], 'hitbox': [12, 24, 36, 48]}
+			'tree': {'url': 'resources/tree.png', 'w': 48, 'h': 48, 'hitbox': [12, 36, 24, 12]},
+			'player': {'url': 'resources/player.png', 'w': 32, 'h': 48, 'cellChange': [16, 36], 'hitbox': [8, 24, 16, 24]},
+			'death': {'url': 'resources/death.png', 'w': 50, 'h': 48, 'cellChange': [25, 36], 'hitbox': [12, 24, 24, 24]}
 		},
 		_nbResources = 4,
 		_tileWidth = 48,
