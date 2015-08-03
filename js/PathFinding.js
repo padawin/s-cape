db59de47eef2c38aa9ce243ff308444a0975a7dd
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-08-03 08:34:19 +0100

    Revert "astar lib used, for the moment"
    This reverts commit 71fb35f91f316114ae74541060e5ffe099c6951a.

--- js/PathFinding.js
+++ js/PathFinding.js
@@ -20,63 +20,53 @@
 			path.push(current);
 		}
 		path.reverse();
-
 		return path;
 	}
 
 	sCape.PathFinding = {
 		shortestPath: function (grid, start, end) {
-			var graph = new Graph(grid.map),
-				start = graph.grid[start.cellY][start.cellX],
-				end = graph.grid[end.cellY][end.cellX],
-				result = astar.search(graph, start, end);
+			var cameFrom = {},
+				costSoFar = {},
+				current,
+				frontier,
+				neighbours,
+				next,
+				nextKey,
+				newCost;
 
-			return result;
+			frontier = new HeapQueue(function(a, b) {
+				return a.priority - b.priority;
+			});
+			start.priority = 0;
 
-			// @XXX @TODO Fix implementation
-			//~var cameFrom = {},
-				//~costSoFar = {},
-				//~current,
-				//~frontier,
-				//~neighbours,
-				//~next,
-				//~nextKey,
-				//~newCost;
-//~
-			//~frontier = new HeapQueue(function(a, b) {
-				//~return a.priority - b.priority;
-			//~});
-			//~start.priority = 0;
-//~
-			//~frontier.push(start);
-			//~cameFrom[_getKey(start)] = null;
-			//~costSoFar[_getKey(start)] = 0;
-//~
-			//~while (frontier.length > 0) {
-				//~current = frontier.pop();
-//~
-				//~if (current == end) {
-					//~break;
-				//~}
-//~
-				//~neighbours = grid.getNeighbours(current);
-				//~newCost = costSoFar[_getKey(current)] + 1;
-				//~for (next in neighbours) {
-					//~if (neighbours[next].content == 'T') {
-						//~continue;
-					//~}
-//~
-					//~nextKey = _getKey(neighbours[next]);
-					//~if (!(nextKey in costSoFar) || newCost < costSoFar[nextKey]) {
-						//~costSoFar[nextKey] = newCost;
-						//~next.priority = newCost + heuristic(end, neighbours[next]);
-						//~frontier.push(neighbours[next]);
-						//~cameFrom[nextKey] = current;
-					//~}
-				//~}
-			//~}
-//~
-			//~return getPath(cameFrom, start, end);
+			frontier.push(start);
+			cameFrom[_getKey(start)] = null;
+			costSoFar[_getKey(start)] = 0;
+
+			while (frontier.length > 0) {
+				current = frontier.pop();
+
+				if (current == end) {
+					break;
+				}
+
+				neighbours = grid.getNeighbours(current);
+				for (next in neighbours) {
+					if (neighbours[next].content == 'T') {
+						continue;
+					}
+
+					nextKey = _getKey(neighbours[next]);
+					newCost = costSoFar[_getKey(current)] + 1;
+					if (!(nextKey in costSoFar) || newCost < costSoFar[nextKey]) {
+						costSoFar[nextKey] = newCost;
+						next.priority = newCost + heuristic(end, neighbours[next])
+						frontier.push(neighbours[next]);
+						cameFrom[nextKey] = current;
+					}
+				}
+			}
+			return getPath(cameFrom, start, end);
 		}
 	};
 
