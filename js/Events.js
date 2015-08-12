8f0c4a94239d64f372cce032cbfbdd982f712ec9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-08-12 14:01:23 +0100

    element added in the events

--- js/Events.js
+++ js/Events.js
@@ -74,22 +74,50 @@
 	var events = {};
 
 	sCape.EventsManager = {
-		on: function(event, callback) {
+		on: function(event, element, callback) {
 			if (!(event in events)) {
 				events[event] = [];
 			}
 
-			events[event].push(callback);
+			events[event].push([element, callback]);
 		},
 
-		off: function (event, callback) {
+		off: function () {
+			var event = null,
+				element = null,
+				callback = null,
+				e = 0;
+			// completely unplug all the events
+			if (arguments.length == 0) {
+				events = {};
+				return;
+			}
+
+			event = arguments[0];
 			if (!(event in events)) {
-				return false;
+				return;
+			}
+
+			// completely unplug the event
+			if (arguments.length == 1) {
+				events[event] = [];
+				return;
+			}
+			// unplug all the events of the given element
+			else if (arguments.length == 2) {
+				element = arguments[1];
+			}
+			// unplug the given event/callback for the given element
+			else if (arguments.length == 3) {
+				element = arguments[1];
+				callback = arguments[2];
 			}
 
 			var e = 0;
 			while (e < events[event].length) {
-				if (callback == events[event][e]) {
+				if (element == events[event][e][0]
+					&& (!callback || callback && callback == events[event][e][1]
+				) {
 					events[event].splice(e, 1);
 				}
 				else {
@@ -98,10 +126,12 @@
 			}
 		},
 
-		fire: function (event) {
+		fire: function (event, element, args) {
 			if (event in events) {
 				for (var e = 0; e < events[event].length; e++) {
-					events[event][e]();
+					if (element == events[event][e][0]) {
+						events[event][e].apply(element, args);
+					}
 				}
 			}
 		}
