25a39611787269764e7b414c343a6d4c6f079804
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-08-14 03:15:37 +0100

    Method to initialize a menu and its events

--- js/Events.js
+++ js/Events.js
@@ -68,6 +68,41 @@
 				});
 				B.addEvent(document, 'mousemove', _clickEvent);
 			}
+		},
+
+		initMenu: function (menu) {
+			function _touchEvent (e) {
+				_clickScreen(true, e.touches[0].clientX, e.touches[0].clientY);
+				e.preventDefault();
+				return false;
+			}
+
+			function _clickEvent (e) {
+				_clickScreen(false, e.clientX, e.clientY);
+			}
+
+			function _clickScreen (isMobile, x, y) {
+				for (var m = 0; m < menu.length; m++) {
+					if (x >= menu[m].coordinates.x && x <= menu[m].coordinates.x + menu[m].coordinates.w
+						&& y >= menu[m].coordinates.y && y <= menu[m].coordinates.y + menu[m].coordinates.h
+					) {
+						sCape.EventsManager.fire('event.clickbutton', menu[m]);
+						if (_isMobile) {
+							B.removeEvent(sCape.GUI.canvas, 'touch', _touchEvent);
+						}
+						else {
+							B.removeEvent(document, 'click', _clickEvent);
+						}
+					}
+				}
+			}
+
+			if (_isMobile) {
+				B.addEvent(sCape.GUI.canvas, 'touch', _touchEvent);
+			}
+			else {
+				B.addEvent(document, 'click', _clickEvent);
+			}
 		}
 	};
 
