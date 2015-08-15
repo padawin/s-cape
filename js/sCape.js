3d67c272d079c8c57eff3988560070f6a5ec0687
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-08-15 08:59:23 +0100

    Modules sytem added in the sCape main module

--- js/sCape.js
+++ js/sCape.js
@@ -1,4 +1,26 @@
-var sCape = {};
+var sCape = (function () {
+	var modules = {},
+	_sCape = {};
+
+	_sCape.addModule = function () {
+		var args = arguments,
+			definition = args.pop(),
+			definitionName = args.shift(),
+			a;
+
+		if (definitionName in modules) {
+			throw 'The module ' + definitionName + ' already exists';
+		}
+
+		for (a = 0; a < args; a++) {
+			args[a] = modules[a];
+		}
+
+		modules[definitionName] = definition.apply({}, args);
+	};
+
+	return _sCape;
+})();
 
 Object.prototype.extends = function (parent) {
 	for (var i in parent) {
