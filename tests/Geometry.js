b85e82e8ec24d0cd4a441486150bcc51064ee242
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2015-08-26 16:55:11 +0100

    More tests added in geometry

--- tests/Geometry.js
+++ tests/Geometry.js
@@ -17,6 +17,14 @@ sCape.executeModule('GeometryTests', 'Geometry', 'Tests', function (Geometry, Te
 			var p = new Geometry.Point(2, 3);
 			Tests.equals(p.x, 2);
 			Tests.equals(p.y, 3);
+
+			p = new Geometry.Point(2);
+			Tests.equals(p.x, 2);
+			Tests.equals(p.y, undefined);
+
+			p = new Geometry.Point();
+			Tests.equals(p.x, undefined);
+			Tests.equals(p.y, undefined);
 		},
 
 		/**
@@ -27,10 +35,76 @@ sCape.executeModule('GeometryTests', 'Geometry', 'Tests', function (Geometry, Te
 		},
 
 		/**
+		 * Test of the method Tests.Rectangle
+		 */
+		function() {
+			var r = new Geometry.Rectangle(1, 2, 3, 4);
+			Tests.equals(r.x, 1);
+			Tests.equals(r.y, 2);
+			Tests.equals(r.w, 3);
+			Tests.equals(r.h, 4);
+
+			r = new Geometry.Rectangle(1, 2, 3);
+			Tests.equals(r.x, 1);
+			Tests.equals(r.y, 2);
+			Tests.equals(r.w, 3);
+			Tests.equals(r.h, undefined);
+
+			r = new Geometry.Rectangle(1, 2);
+			Tests.equals(r.x, 1);
+			Tests.equals(r.y, 2);
+			Tests.equals(r.w, undefined);
+			Tests.equals(r.h, undefined);
+
+			r = new Geometry.Rectangle(1);
+			Tests.equals(r.x, 1);
+			Tests.equals(r.y, undefined);
+			Tests.equals(r.w, undefined);
+			Tests.equals(r.h, undefined);
+
+			r = new Geometry.Rectangle();
+			Tests.equals(r.x, undefined);
+			Tests.equals(r.y, undefined);
+			Tests.equals(r.w, undefined);
+			Tests.equals(r.h, undefined);
+		},
+
+		/**
 		 * Test if the method Tests.Segment exists
 		 */
 		function() {
 			Tests.isA(Geometry.Segment, 'function');
+		},
+
+		/**
+		 * Test of the method Tests.Segment
+		 */
+		function() {
+			var s = new Geometry.Segment(new Geometry.Point(1, 2), new Geometry.Point(3, 4));
+			Tests.equals(s.p1.x, 1);
+			Tests.equals(s.p1.y, 2);
+			Tests.equals(s.p2.x, 3);
+			Tests.equals(s.p2.y, 4);
+
+			s = new Geometry.Segment(new Geometry.Point(1, 2), new Geometry.Point(3));
+			Tests.equals(s.p1.x, 1);
+			Tests.equals(s.p1.y, 2);
+			Tests.equals(s.p2.x, 3);
+			Tests.equals(s.p2.y, undefined);
+
+			s = new Geometry.Segment(new Geometry.Point(1, 2));
+			Tests.equals(s.p1.x, 1);
+			Tests.equals(s.p1.y, 2);
+			Tests.equals(s.p2, undefined);
+
+			s = new Geometry.Segment(new Geometry.Point(1));
+			Tests.equals(s.p1.x, 1);
+			Tests.equals(s.p1.y, undefined);
+			Tests.equals(s.p2, undefined);
+
+			s = new Geometry.Segment();
+			Tests.equals(s.p1, undefined);
+			Tests.equals(s.p2, undefined);
 		}
 	]);
 });
